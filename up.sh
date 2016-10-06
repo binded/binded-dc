b0f64136b27cbdb643d287014b3fdc271360034f
Olivier Lalonde <olalonde@gmail.com>
2016-10-06 03:34:52 -0700

    fix up.sh

--- up.sh
+++ up.sh
@@ -1,2 +1,4 @@
-#!/bin/sh
-docker-compose up -d -p blockai
+#!/bin/bash
+basepath=$(dirname "$BASH_SOURCE")
+dcfile="$basepath/docker-compose.yml"
+docker-compose --project-name blockai -f "$dcfile" up -d
